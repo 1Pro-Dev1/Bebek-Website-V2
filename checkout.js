d774e4ffe736096d04f847fc4c2ca0123bf8e910
1Mohamed-Dev <dude.alt.mail159@gmail.com>
2022-11-30 15:20:15 +0400

    Imgs problem solved

--- checkout.js
+++ checkout.js
@@ -9,9 +9,26 @@ if (localStorage.getItem("products") == null){
     let productsStorage = localStorage.getItem("products");
     let productsArray = productsStorage.split(",")
     var productsContainer = document.querySelector(".products")
+    var totalPrice = 0;
     for (let i = 0; i < productsArray.length; i++) {
         const e = productsArray[i];
         elementFactory(e)
+        if (i == productsArray.length - 1){
+            let total = document.createElement("div")
+            total.classList.add("card", "p-3", "total")
+
+            let totalH = document.createElement("h3")
+            totalH.innerText = "Total:"
+            totalH.classList.add("m-0")
+            
+            let totalSpan = document.createElement("span")
+            totalSpan.innerText = `${totalPrice}.00 EGP`;
+            totalSpan.classList.add("text-main", "fs-4")
+            
+            total.appendChild(totalH);
+            total.appendChild(totalSpan);
+            productsContainer.appendChild(total);
+        }
     }
     
     function elementFactory(productName) {
@@ -58,35 +75,38 @@ if (localStorage.getItem("products") == null){
         productsContainer.appendChild(mainCard)
     
         if (productName == "product1"){
-            colImg.src = "/imgs/product.webp"
+            colImg.src = "imgs/product.webp"
             cardtitle.innerText = "Large Baby Bottle 240mm"
             carddesc.innerText = `Keep your baby hydrated using Bebek's special 
             bottle with soft flexible nipple for high-quality healthy breastfeeding.`
-            pricesm.innerText = `Price: 004 EGP`
+            pricesm.innerText = `Price: 600.00 EGP`
+            totalPrice += 600;
     
         } else if(productName == "product2"){
-            colImg.src = "/imgs/product.webp"
+            colImg.src = "imgs/product.webp"
             cardtitle.innerText = "Small Baby Bottle 180mm"
             carddesc.innerText = `Premium BPA-free plastic is perfect for your
             toddler's health since BPA is identified as harmful to babies' growth.`
-            pricesm.innerText = `Price: 003 EGP`
+            pricesm.innerText = `Price: 500.00 EGP`
+            totalPrice += 500;
     
         } else if(productName == "product3"){
-            colImg.src = "/imgs/product2.webp"
+            colImg.src = "imgs/product2.webp"
             cardtitle.innerText = "Soft Silicone Feeding Nipple"
             carddesc.innerText = `With anti-gas and anti-colic features, Bebek's
             soft nipple will help your newborn to calm down and sleep well.`
-            pricesm.innerText = `Price: 002 EGP`
+            pricesm.innerText = `Price: 300.00 EGP`
+            totalPrice += 300;
     
         } else if(productName == "product4"){
-            colImg.src = "/imgs/product3.webp"
+            colImg.src = "imgs/product3.webp"
             cardtitle.innerText = "Toddler Pacifier"
             carddesc.innerText = `Bebek's pacifier is supported with a clip to prevent 
             it from falling and getting dirty. It can also be easily cleaned or sterilized.`
-            pricesm.innerText = `Price: 003 EGP`
+            pricesm.innerText = `Price: 300.00 EGP`
+            totalPrice += 300;
     
         }
-
     }
 }
 
@@ -96,3 +116,25 @@ checkoutbtn.addEventListener("click", () =>{
     checkoutbtn.style.display = "none"
     localStorage.clear()
 })
+
+// Payment choose //
+
+
+// let paymentBox = document.querySelector(".checkout .payment");
+// let paymentBtns = document.querySelectorAll(".checkout .payment span");
+// console.log(paymentBtns)
+// paymentBtns[0 || 1].addEventListener("click", (e) =>{
+//     if (paymentType == null || paymentType == undefined || paymentType == ""){
+//         paymentType = e.target.getAttribute("data-kind");
+//     } else {
+//         var paymentType = e.target.getAttribute("data-kind");
+//     }
+//     for (let i = 0; i < paymentBtns.length; i++) {
+//         const ele = paymentBtns[i];
+//         if (ele.classList.contains("active")){
+//             ele.classList.remove("active");
+//         }
+//     }
+//     console.log(e.target)
+//     e.target.classList.add("active");
+// })
